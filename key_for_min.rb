afbaee232c904691d45e6dc11099a095ee5c61ce
Lauren Frankovich <frankovister@gmail.com>
2020-05-27 04:36:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,8 @@
 #hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  if name_hash = {}
   smallest_value = 1000
-  smallest_name = ""
+  smallest_name = nil
   name_hash.each do |name, num|
     if num < smallest_value
       smallest_value = num
